877959cded7f29e898e9b887e6c73e48f5799316
TonyKwan <tonykwan93@gmail.com>
2016-11-29 11:52:13 +0100

    Revert "Revert "First commit, bitch""
    This reverts commit c5fa114b4094b0b40b65187c3778d16bdbae3b20.

---
+++ MazeDrawer/HelperClasses/Autobot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeDrawer
+{
+    class Autobot
+    {
+        private Orientation orientation;
+        private Orientation lastOrientation;
+
+        public Autobot(Orientation orientation)
+        {
+            this.orientation = orientation;
+        }
+
+        public Orientation Orientation { get { return this.orientation; } set { this.orientation = value; } }
+        public Orientation LastOrientation { get {return this.lastOrientation; } set {this.lastOrientation = value; } }
+    }
+}
